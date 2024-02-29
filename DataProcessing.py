5b5aaac77b48a7c82d7ee2bb8635370c11f0ab64
Yoithebrain <crazyone5477@gmail.com>
2024-02-29 12:55:43 +0100

    This won't work ARGGHH

--- DataProcessing.py
+++ DataProcessing.py
@@ -22,11 +22,12 @@ def get_n_lines (n_of_lines):
     for line in reader.read_x_lines(n_of_lines):
         print(line)
 '''
-# Function for read file process, takes a queue and a filepath. Queue is used to allow the second process to process the line
-def read_file (reader, queue):
+# Function for read file process, takes a reader. Queue is used to allow the second process to process the line
+def read_file(reader, queue):
     for line in reader.read_lines():
         queue.put(line)
     queue.put("END")
+
 
 def process_lines(queue, reader):
     line_count = 0
@@ -46,8 +47,38 @@ def main ():
     pr = cProfile.Profile()
     pr.enable()
     reader = FileReader('files\\really_big_file_with_errors.txt')
-    # The number of proccesors
-    # num_of_processors = 2
+    # The number of proccesors, now based on cpu count
+    num_of_processors = multiprocessing.cpu_count()
+    # Creation of a pool of processors
+    pool = multiprocessing.Pool(processes=num_of_processors)
+
+
+    # Creates queue for processors to communicate between and a manager to facilitate this due to making use of a pool to avoid
+    # run time errors
+    manager = multiprocessing.Manager()
+    queue = manager.Queue()
+    # Making use of the pool to help speed things up
+    pool.apply_async(read_file, args=(reader, queue))
+    pool.apply_async(process_lines, args=(queue, reader))
+
+
+
+    #pool.close()
+    #pool.join()
+    
+    '''
+    # Create a process that reads the file
+    reader_process = multiprocessing.Process(target=read_file, args=(reader, queue))
+    reader_process.start()
+    # Create a process that handles processing the lines from the queue
+    line_processing_process = multiprocessing.Process(target=process_lines, args=(queue, reader))
+    line_processing_process.start()
+
+    # Wait for the processors to finish
+    reader_process.join()
+    line_processing_process.join()
+    '''
+    '''
     ###
     # This code turned out to be slower, issue might be due to the fact that the queue fills faster than it can be emptied.
     # - CLY 290224 -
@@ -66,7 +97,7 @@ def main ():
     # Wait for the processors to finish
     reader_process.join()
     line_processing_process.join()
-   
+   '''
     pr.disable()
     pr.print_stats(sort='cumulative')
 
